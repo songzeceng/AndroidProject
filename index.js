6ba35721a81461f43ba49e7aab702479215c04ac
songzeceng <songzeceng@bytedance.com>
2018-06-19 12:21:42 +0800

    文本输入

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {AppRegistry, Image, StyleSheet, Text, View} from 'react-native';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TextInput,
+    View
+} from 'react-native';
 
 const styles = StyleSheet.create({
     bigBlue: {
@@ -41,6 +47,39 @@ class Blink extends Component {
     }
 }
 
+class CityInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ""};
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder={"输入你要去的城市"} // 占位字符串
+                    // onChangeText={ // 输入内容有变时调用
+                    //     (text) => this.setState({text})
+                    // }
+                    onSubmitEditing={ // 按下软键盘上的确认键后调用，参数是event
+                        (event) => this.setState({text:event.nativeEvent.text})
+                        // 通过调用event.nativeEvent.text获取输入的字符串
+                    }
+                ></TextInput>
+
+                <Text style={{padding: 10, fontSize: 30}}>
+                    {
+                        this.state.text.split('-').map( // split函数：分割，map函数：转换
+                            (str) => "那就去" + str
+                        ).join('\n') // 字符串拼接
+                    }
+                </Text>
+            </View>
+        );
+    }
+}
+
 /**
  * width和height可以指定元素的尺寸，RN的尺寸是没有单位的逻辑像素，和设备无关
  * 当style以数组的形式设定时，后面的元素会覆盖前面相同类型的元素
@@ -58,21 +97,22 @@ class AwesomeProject extends Component {
                 // flex是权重
                 flexDirection: 'row',
                 // flexDirection是排列方向，排成一列column或者排成一排row
-                flexWrap:'wrap',
+                flexWrap: 'wrap',
                 // flexWrap:内容包裹形式。nowrap:不换行，此为默认 wrap:换行，wrap-reverse:换行，第一行在下面
                 justifyContent: 'space-around'
                 // justifyContent意为内容的分布方式，可选项有：flex-start、center、flex-end、space-around、space-between
                 // 以横向排列为例，意思依次是左对齐，居中，右对齐，每个子项两侧间隔相等(所以不会靠两侧)，每个子项之间的间隔相等(所以会靠两侧)
             }}>
-                <Image source={pic} style={{width: 200, height: 200}}/>
-                <City name='安阳' fontSize={50}/>
-                <City name='新乡' fontSize={30}/>
-                <City name='洛阳' fontSize={20}/>
-                <Blink/>
-                <Text style={styles.red}>红色</Text>
-                <Text style={styles.bigBlue}>蓝色大号</Text>
-                <Text style={[styles.red, styles.bigBlue]}>红色、蓝色大号</Text>
-                <Text style={[styles.bigBlue, styles.red]}>蓝色大号、红色</Text>
+                {/*<Image source={pic} style={{width: 200, height: 200}}/>*/}
+                {/*<City name='安阳' fontSize={50}/>*/}
+                {/*<City name='新乡' fontSize={30}/>*/}
+                {/*<City name='洛阳' fontSize={20}/>*/}
+                {/*<Blink/>*/}
+                {/*<Text style={styles.red}>红色</Text>*/}
+                {/*<Text style={styles.bigBlue}>蓝色大号</Text>*/}
+                {/*<Text style={[styles.red, styles.bigBlue]}>红色、蓝色大号</Text>*/}
+                {/*<Text style={[styles.bigBlue, styles.red]}>蓝色大号、红色</Text>*/}
+                <CityInput/>
             </View>
         );
     }
