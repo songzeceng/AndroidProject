7c9d7ecac4a94768a4032954a78e3bf35a24345d
songzeceng <392004248@qq.com>
2019-08-10 12:03:08 +0800

    用路由返回结果

--- flutter_tutorial/lib/route.dart
+++ flutter_tutorial/lib/route.dart
@@ -26,6 +26,12 @@ class PageA extends StatelessWidget {
         title: Text("A页面"),
       ),
       body: new Text("红军利物浦"),
+      floatingActionButton: new FloatingActionButton(onPressed: () {
+        Navigator.of(context).pop({
+          "主教练": "克洛普",
+          "队长": "乔丹·亨德森"
+        });
+      }, child: Icon(Icons.arrow_back),),
     );
   }
 }
@@ -39,6 +45,12 @@ class PageB extends StatelessWidget {
         title: Text("B页面"),
       ),
       body: new Text("蓝月曼城"),
+      floatingActionButton: new FloatingActionButton(onPressed: () {
+        Navigator.of(context).pop({
+          "主教练": "佩普·瓜迪奥拉",
+          "队长": "孔帕尼"
+        });
+      }, child: Icon(Icons.arrow_back),),
     );
   }
 }
@@ -52,6 +64,12 @@ class PageC extends StatelessWidget {
         title: Text("C页面"),
       ),
       body: new Text("南部之星拜仁"),
+      floatingActionButton: new FloatingActionButton(onPressed: () {
+        Navigator.of(context).pop({
+          "主教练": "尼科·科瓦奇",
+          "队长": "诺伊尔"
+        });
+      }, child: Icon(Icons.arrow_back),),
     );
   }
 }
@@ -66,6 +84,27 @@ class PageNavigator extends StatefulWidget {
 
 class PageNavigatorState extends State<PageNavigator> {
   List<String> mItems = ["A", "B", "C"];
+
+  void updateList(int index, String newValue) {
+    setState(() {
+      mItems[index] = newValue;
+    });
+  }
+
+  String map2Str(Map map) {
+    StringBuffer buffer = new StringBuffer();
+    List<String> keys = map.keys.toList();
+    List<String> values = map.values.toList();
+    for (int i = 0; i < keys.length; i++) {
+      String key = keys[i];
+      String value = values[i];
+      buffer.write(key + ":" + value + "\n");
+    }
+
+    String retStr = buffer.toString();
+    return retStr.substring(0, retStr.lastIndexOf("\n"));
+  }
+
   getView() {
     return ListView.builder(
         padding: const EdgeInsets.all(5.0),
@@ -81,8 +120,11 @@ class PageNavigatorState extends State<PageNavigator> {
           String currentItem = mItems[index];
           return new ListTile(
             title: Text(currentItem),
-            onTap: () {
-              Navigator.of(context).pushNamed(currentItem);
+            onTap: () async {
+              Map result = await Navigator.of(context).pushNamed(currentItem) as Map;
+              if (result != null) {
+                updateList(index, map2Str(result));
+              }
             },
           );
           
