13539050236ab17bdc13214f6cd8ee52f3bf6579
songzeceng <392004248@qq.com>
2019-09-15 16:51:22 +0800

    动态代理与hook

--- AndroidPractice/app/src/main/java/com/example/songzeceng/androidpractice/BaseUtils.java
+++ AndroidPractice/app/src/main/java/com/example/songzeceng/androidpractice/BaseUtils.java
@@ -1,5 +1,6 @@
 package com.example.songzeceng.androidpractice;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +8,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class BaseUtils {
     //获取手机类型
@@ -29,6 +36,45 @@ public class BaseUtils {
             Log.e("HLQ_Struggle", e.getLocalizedMessage());
             intent = new Intent(Settings.ACTION_SETTINGS);
             context.startActivity(intent);
+        }
+    }
+
+    /**
+     * hook住OnClick方法
+     * view.setOnClickListener()方法会给mListenerInfo对象里的mOnClickListener属性赋值
+     * mListenerInfo对象通过getListenerInfo()方法获取
+     *
+     * @param context 上下文，提供动态代理的classLoader
+     * @param view 被hook的view对象
+     */
+    public static void hookOnClick(Context context, View view) {
+        try {
+            // 1、获取mListenerInfo对象
+            Method method = View.class.getDeclaredMethod("getListenerInfo");
+            method.setAccessible(true);
+            Object listenerInfo = method.invoke(view);
+
+            // 2、获取mListener对象的mOnClickListener属性
+            Class<?> listenerInfoClass = listenerInfo.getClass();
+            Field field = listenerInfoClass.getField("mOnClickListener");
+            final View.OnClickListener onClickListener = (View.OnClickListener) field.get(listenerInfo);
+
+            // 3、通过动态代理，实例化代理对象
+            Object proxyClickListener = Proxy.newProxyInstance(context.getClassLoader(),
+                    new Class[]{View.OnClickListener.class}, new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    // 执行新的逻辑(埋点、输出日志等)
+                    Log.i("MainActivity", "invoke: view is clicked");
+                    // 执行老的逻辑
+                    return method.invoke(onClickListener, args);
+                }
+            });
+
+            // 4、更新mOnClickListener属性
+            field.set(listenerInfo, proxyClickListener);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- AndroidPractice/app/src/main/java/com/example/songzeceng/androidpractice/MainActivity.java
+++ AndroidPractice/app/src/main/java/com/example/songzeceng/androidpractice/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     private SmsManager mSmsManager = SmsManager.getDefault();
@@ -28,11 +31,26 @@ public class MainActivity extends Activity {
                 && checkSelfPermission("android.permission.READ_PHONE_NUMBERS") != PackageManager.PERMISSION_GRANTED
                 && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
                 || checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
-                || checkSelfPermission(Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED) {
-            BaseUtils.jumpStartInterface(this);
+                || checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.READ_SMS, "android.permission.READ_PHONE_NUMBERS",
+                    Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS}, 0);
+
             return;
         }
         sendMessage();
+
+
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "onClick: button is clicked");
+            }
+        });
+
+        BaseUtils.hookOnClick(this, btn); // 要在被hook的方法之后进行hook，否则会被老的方法覆盖
     }
 
     @SuppressLint("MissingPermission")
@@ -42,5 +60,17 @@ public class MainActivity extends Activity {
 
         mSmsManager.sendTextMessage(mDesPhoneNumber, mSrcPhoneNumber, "hello From AS",
                 null, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if ((checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission("android.permission.READ_PHONE_NUMBERS") != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+                || checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            BaseUtils.jumpStartInterface(this);
+        }
     }
 }
